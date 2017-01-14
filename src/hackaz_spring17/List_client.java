3ed386771ade4b718c6c975c4969ff1e8058d967
Niven Francis <nivenf@email.arizona.edu>
2017-01-14 12:32:51 -0700

    maybe?

---
+++ src/hackaz_spring17/List_client.java
@@ -0,0 +1,30 @@
+package hackaz_spring17;
+
+import java.util.ArrayList;
+
+public class List_client {
+	
+	ArrayList<String> li;
+	
+	public boolean add() {
+		
+	}
+	
+	public boolean remove() {
+		
+	}
+	
+	public boolean swap() {
+		
+	}
+	
+	public boolean moveToTop() {
+		
+	}
+	
+	public String listString() {
+		
+	}
+	
+	
+}
--- src/hackaz_spring17/Test.java
+++
@@ -1,7 +0,0 @@
-package hackaz_spring17;
-
-public class Test {
-	Hi
-	what's up
-	hi
-}
