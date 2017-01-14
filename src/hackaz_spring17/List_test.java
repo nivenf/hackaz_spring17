28fd7dffd5dc75c217498434cb362b40777444fb
Niven Francis <nivenf@email.arizona.edu>
2017-01-14 13:14:38 -0700

    added tester - nivenf

---
+++ src/hackaz_spring17/List_test.java
@@ -0,0 +1,70 @@
+package hackaz_spring17;
+
+import java.util.Scanner;
+
+public class List_test {
+	static Scanner keyboard;
+	static List_client li;
+	
+	public static void main(String[] args) {
+		keyboard = new Scanner(System.in);
+		li = new List_client();
+		System.out.println("Current list: " + li.listString());
+		
+		System.out.println("What would you like to do: add/remove/swap/move");
+		String input = keyboard.next();
+		System.out.println(input);
+		
+		if(input.equals("add"))
+			add();
+		else if(input.equals("remove"))
+			remove();
+		else if(input.equals("swap"))
+			swap();
+		else if(input.equals("move"))
+			move();
+		else
+			System.out.println("Usage: Please type a command: add/remove/swap/move");
+	}
+	
+	public static void add() {
+		System.out.print("Enter what you would like to add: ");
+		String in = keyboard.next();
+		
+		if(li.add(in))
+			System.out.println("Current list: " + li.listString());
+		else
+			System.out.println("Couldn't add. Current list: " + li.listString());
+	}
+	
+	public static void remove() {
+		System.out.print("Which index would you like to remove (starting at 0)?");
+		int in = Integer.parseInt(keyboard.next());
+		
+		if(li.remove(in))
+			System.out.println("Current list: " + li.listString());
+		else
+			System.out.println("Couldn't remove. Current list: " + li.listString());
+	}
+	
+	public static void swap() {
+		System.out.println("What would you like to swap? Enter two integers");
+		int in1 = Integer.parseInt(keyboard.next());
+		int in2 = Integer.parseInt(keyboard.next());
+		
+		if(li.swap(in1, in2))
+			System.out.println("Current list: " + li.listString());
+		else
+			System.out.println("Couldn't swap. Current list: " + li.listString());
+	}
+	
+	public static void move() {
+		System.out.println("What would you like to move to top? Enter an integer");
+		int in = Integer.parseInt(keyboard.next());
+		
+		if(li.moveToTop(in))
+			System.out.println("Current list: " + li.listString());
+		else
+			System.out.println("Couldn't swap. Current list: " + li.listString());
+ 	}
+}
