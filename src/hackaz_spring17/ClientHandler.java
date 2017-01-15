ac1ed1edbfde2a0c5f3347d7e879745f9e065ce3
Megan Rossier <meganrossier@Megans-MacBook-Pro.local>
2017-01-14 18:08:52 -0700

    client handler

--- src/hackaz_spring17/ClientHandler.java
+++ src/hackaz_spring17/ClientHandler.java
@@ -1,5 +1,6 @@
 package hackaz_spring17;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.List;
@@ -8,4 +9,38 @@ import java.util.List;
 public class ClientHandler extends Thread {
 	private ObjectInputStream input;
 	private List<ObjectOutputStream> clients;
+	
+	public ClientHandler(ObjectInputStream input, List<ObjectOutputStream> clients){
+		this.input = input;
+		this.clients = clients;
+	}
+	
+	@Override
+	public void run(){
+		while(true){
+			String message = null;
+			
+			try{
+				message = (String) input.readObject();
+			}catch (Exception e) {
+				e.printStackTrace(); 
+			}
+			this.writeStringToClients(message);
+		}
+	}
+
+	private void writeStringToClients(String message) {
+		synchronized (clients){
+			for(ObjectOutputStream client : clients){
+				try {
+					client.writeObject(message);
+					client.reset();
+				} catch(IOException e) {
+					e.printStackTrace();
+					clients.remove(client);
+				}
+			}
+		}
+	}
+	
 }
