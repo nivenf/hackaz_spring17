e2988dc75b37dec967fef40c460b16d5c03154c1
Jenna Dornquast <Jenna Dornquast>
2017-01-14 18:16:16 -0700

    server stuff

--- src/hackaz_spring17/Server.java
+++ src/hackaz_spring17/Server.java
@@ -15,15 +15,33 @@ public class Server {
 	
 	private static ServerSocket socket;
 	
-	 while (true) {
-	      Socket s = socket.accept();
-
-	      ObjectInputStream inputStream = new ObjectInputStream(s.getInputStream());
-	      ObjectOutputStream outputStream = new ObjectOutputStream(s.getOutputStream());
-=======
 	private static List<ObjectOutputStream> clients =
 			Collections.synchronizedList(new ArrayList<>());
 			
-	socket = new ServerSocket(SERVER_PORT);
+	
+	public static void main(String[] args) throws IOException {
+		socket = new ServerSocket(Server_Port);
+		System.out.println("Server up and running successfully. /n Port number "
+		+ Server_Port + " being used.");
+		
+		while(true){
+			Socket sock = socket.accept();
+			
+			ObjectInputStream inputStream = new ObjectInputStream(sock.getInputStream());
+			ObjectOutputStream outputStream = new ObjectOutputStream(sock.getOutputStream());
+			
+			clients.add(outputStream);
+			
+			ClientHandler manager = new ClientHandler(inputStream, clients);
+			manager.start();
+			
+			System.out.println("New connection accepted onto server from " 
+			+ socket.getInetAddress());
+		}
+	}
+	
+	
+	
+	
 	
 }
