7312f12c787889b3198e73faf819843ae863c2a4
Jenna Dornquast <Jenna Dornquast>
2017-01-14 17:34:48 -0700

    socket?

--- src/hackaz_spring17/Server.java
+++ src/hackaz_spring17/Server.java
@@ -1,5 +1,20 @@
 package hackaz_spring17;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Server {
 
+	public static final int Server_Port = 7;
+	
+	private static ServerSocker socket;
+	
+	
+	
 }
